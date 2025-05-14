e52bde547fdd0ae7b119a41fabe5c073cfc95ab6
cqjro <cairo.cristante@gmail.com>
2025-05-14 19:00:58 -0400

    home-manager works fr this time

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     nixos-hardware.url = "github:nixos/nixos-hardware";
 
     home-manager = {
-      url = "github:nix-community/home-manager/master";
+      url = "github:nix-community/home-manager/";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
--- hosts/LittleNix/configuration.nix
+++ hosts/LittleNix/configuration.nix
@@ -8,7 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      inputs.home-manager.nixosModules.default
+      # inputs.home-manager.nixosModules.default
       # inputs.xremap-flake.nixosModules.default
     ];
 
