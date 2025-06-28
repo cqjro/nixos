6215706f46193e9a2a58d050a3878767c6e0ce97
cqjro <cairo.cristante@gmail.com>
2025-06-28 19:52:32 -0400

    LittleNix: 166 current  2025-06-28 19:52:26  25.05.20250513.adaa24f  6.12.28                          * || fixing lsp?

--- modules/home-manager/terminal/nvim/lsp/lsp.lua
+++ modules/home-manager/terminal/nvim/lsp/lsp.lua
@@ -1,12 +1,33 @@
+vim.lsp.enable({
+    "gopls",
+    "lua_ls",
+		"nixd",
+		"pyright",
+		"rust-analyzer",
+		"texlab",
+		"ts_lua",
+})
 
-vim.lsp.enable('lua-language-server')
-
-vim.lsp.enable('pyright')
-
-vim.lsp.enable('nixd')
-
-vim.lsp.enable('rust-analyzer')
-
-vim.lsp.enable('gopls')
-
-vim.lsp.enable('texlab')
+vim.diagnostic.config({
+    virtual_lines = true,
+    -- virtual_text = true,
+    underline = true,
+    update_in_insert = false,
+    severity_sort = true,
+    float = {
+        border = "rounded",
+        source = true,
+    },
+    signs = {
+        text = {
+            [vim.diagnostic.severity.ERROR] = "󰅚 ",
+            [vim.diagnostic.severity.WARN] = "󰀪 ",
+            [vim.diagnostic.severity.INFO] = "󰋽 ",
+            [vim.diagnostic.severity.HINT] = "󰌶 ",
+        },
+        numhl = {
+            [vim.diagnostic.severity.ERROR] = "ErrorMsg",
+            [vim.diagnostic.severity.WARN] = "WarningMsg",
+        },
+    },
+})
