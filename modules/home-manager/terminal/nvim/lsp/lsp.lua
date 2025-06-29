53a7149ef09348086d1e107bb19e4ac6ae7401d4
cqjro <cairo.cristante@gmail.com>
2025-06-29 16:16:03 -0400

    LittleNix: 168 current  2025-06-29 16:15:55  25.05.20250513.adaa24f  6.12.28                          * || attempting to fix the lua lsp stuff

--- modules/home-manager/terminal/nvim/lsp/lsp.lua
+++ modules/home-manager/terminal/nvim/lsp/lsp.lua
@@ -8,6 +8,15 @@ vim.lsp.enable({
 		"ts_ls",
 })
 
+-- Importing the configuration files for each lsp
+require("lua_ls.lua")
+require("gopls.lua")
+require("nixd.lua")
+require("pyright.lua")
+require("rust_analyzer.lua")
+require("texlab.lua")
+require("ts_ls.lua")
+
 vim.diagnostic.config({
     virtual_lines = true,
     -- virtual_text = true,
