6c21aa87f6fa308c3743b6d4794756a4a82bfa26
cqjro <cairo.cristante@gmail.com>
2025-07-09 19:47:09 -0400

    LittleNix: 199 current  2025-07-09 19:46:59  25.05.20250513.adaa24f  6.12.28                          * || adding proton-mail.desktop configuration

--- modules/home-manager/desktop/xdg-desktop-entries.nix
+++ modules/home-manager/desktop/xdg-desktop-entries.nix
@@ -1,16 +1,28 @@
-{pkgs, lib, config, inputs, ...}:
+{...}:
 {
 	xdg.desktopEntries = {
 		vesktop = {	
-			name="Vesktop";
-			categories =["Network" "InstantMessaging" "Chat"];
-			exec ="vesktop --enable-features=WebRTCPipeWireCapturer --ozone-platform-hint=wayland %U";
-			genericName="Internet Messenger";
-			icon ="vesktop";
+			name = "Vesktop";
+			categories = ["Network" "InstantMessaging" "Chat"];
+			exec = "vesktop --enable-features=WebRTCPipeWireCapturer --ozone-platform-hint=wayland %U";
+			genericName = "Internet Messenger";
+			icon = "vesktop";
 			# keywords = ["discord" "vencord" "electron" "chat"];
 			# startupWMClass = "Vesktop";
 			type = "Application";
 			# version = 1.4;
 		};
+
+		proton-mail = {
+			name = "Proton Mail";
+			comment = "Proton official desktop application for Proton Mail and Proton Calendar";
+			genericName = "Proton Mail";
+			exec = "proton-mail --ozone-platform-hint=wayland %U";
+			icon = "proton-mail";
+			type = "Application";
+			# startupNotify = "true";
+			categories = ["Network" "Email"];
+			# mimeType="x-scheme-handler/mailto";
+		};
 	};
-}	
+}
