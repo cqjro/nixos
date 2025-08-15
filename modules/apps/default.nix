173f2de969973e2c67779e1d6c52bcb0caefc90b
cqjro <cairo.cristante@gmail.com>
2025-08-14 21:01:23 -0400

    GOD-KILLER: 18          2025-08-14 21:01:20  25.11.20250716.6e98748     6.15.6  Unknown                 []              True || installing gpclient

--- modules/apps/default.nix
+++ modules/apps/default.nix
@@ -24,5 +24,6 @@
 		newsflash # RSS reader/news app
 		onlyoffice-desktopeditors # microsoft suite replacement
 		openconnect # vpn client that might work with uoft vpn
+		gpclient # gui that supports sso?
 	];
 }
