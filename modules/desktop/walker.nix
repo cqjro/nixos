b581b81aaf1027147887a372fdb1d0854056fc36
cqjro <github@cqjro.ca>
2026-03-08 15:25:21 -0400

    LittleNix: 260         2026-03-08 15:25:12  26.05.20260223.2fc6539  6.12.74  Unknown                 []              True || styling walker with stylix

--- modules/desktop/walker.nix
+++ modules/desktop/walker.nix
@@ -1,4 +1,4 @@
-{inputs, ...}:
+{inputs, config, ...}:
 {
 
 imports = [
@@ -7,6 +7,25 @@ imports = [
 
 	programs.walker = {
 		enable = true;
-		# package = pkgs.walker;
+		themes."mytheme".style = ''
+    window {
+      background-color: transparent;
+    }
+
+    #window {
+      background-color: #${config.lib.stylix.colors.base00};
+      /* border-radius: 12px; */
+      border: 1px solid #${config.lib.stylix.colors.base03};
+    }
+
+    #input {
+      color: #${config.lib.stylix.colors.base05};
+      background-color: #${config.lib.stylix.colors.base01};
+    }
+
+    #typeahead {
+      color: #${config.lib.stylix.colors.base03};
+    }
+  '';
 	};
 }
