33ba5aeb1fabc8e605b617cf78fe9026064dfd3d
cqjro <github@cqjro.ca>
2026-05-01 19:02:23 -0400

    GOD-KILLER: 59          2026-05-01 19:02:20  26.05.20260427.1c3fe55     7.0.1   Unknown                 []              True || fixing neovim issue

--- modules/terminal/nvim/neovim.nix
+++ modules/terminal/nvim/neovim.nix
@@ -6,8 +6,8 @@
 
 	programs.neovim = 
 		let
-			toLua = str: "lua << EOF\n${str}\nEOF\n";
-			toLuaFile = file: "lua << EOF\n${builtins.readFile file}\nEOF\n";
+			toLua = str: str;
+			toLuaFile = file: builtins.readFile file;
 			fakeVimPlugin = pkgs.runCommand "fakeVimPlugin" {} "mkdir $out";
 		in
 			{
