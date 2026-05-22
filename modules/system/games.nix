1a2b8e41b807b0091b43a8b026043a2c24ea277b
cqjro <github@cqjro.ca>
2026-05-22 08:34:25 -0400

    GOD-KILLER: 89          2026-05-22 08:34:23  26.05.20260427.1c3fe55  7.0.1   Unknown                 []              True || fixing steam controller firmware update issues

--- modules/system/games.nix
+++ modules/system/games.nix
@@ -23,6 +23,10 @@
 
 		programs.steam = {
 			enable = true;
+			extest.enable = true;
+			extraPackages = with pkgs; [
+				hidapi # fixes steam controller firmware update issues
+			];
 			# gamescopeSession.enable = true; # starts game in a microcompositor if there is resolution/upscaling issues on DE/window manager
 		};
 
