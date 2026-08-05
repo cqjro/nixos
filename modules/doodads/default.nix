e795934caffe7d0b394b47be9a163ea67d4d07d0
cqjro <github@cqjro.ca>
2026-08-05 16:45:37 -0400

    GOD-KILLER: 118         2026-08-05 16:45:35  26.11.20260705.d407951  7.1.3   Unknown                 []              True || installing soulseek apps

--- modules/doodads/default.nix
+++ modules/doodads/default.nix
@@ -4,7 +4,8 @@
 environment.systemPackages = with pkgs; [
 	mtkclient # installing firmware, like the innioasis Y1
 	rockbox-utility # open source firmware for digital audio players
-
+	slsk-batchdl # batch installer for soulseek
+	nicotine-plus # GUI for soulseek
 ];
 
 }
