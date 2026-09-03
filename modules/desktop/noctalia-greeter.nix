3608699d73277b90eb3bbf620ccd3c411bff04ad
cqjro <github@cqjro.ca>
2026-09-03 17:20:46 -0400

    GOD-KILLER: 156         2026-09-03 17:20:43  26.11.20260828.83199d0  7.2.1   Unknown                 []              True || applying stylix colours to noctalia greeter

--- modules/desktop/noctalia-greeter.nix
+++ modules/desktop/noctalia-greeter.nix
@@ -1,4 +1,7 @@
-{ inputs, pkgs, ...}:
+{ inputs, pkgs, config, ...}:
+let
+	c = config.lib.stylix.colors.withHashtag;
+in
 {
 	imports = [
 		inputs.noctalia-greeter.nixosModules.default
@@ -19,6 +22,25 @@
 			keyboard = {
 				layout = "us";
 			};
+
+			appearance.palette = {
+        primary            = c.base0D; # blue/accent
+        on_primary         = c.base00; # bg
+        secondary          = c.base0C; # cyan
+        on_secondary       = c.base00;
+        tertiary           = c.base0B; # green
+        on_tertiary        = c.base00;
+        error              = c.base08; # red
+        on_error           = c.base00;
+        surface            = c.base00; # bg
+        on_surface         = c.base05; # fg
+        surface_variant    = c.base01; # bg1
+        on_surface_variant = c.base04; # fg dim
+        outline            = c.base03;
+        shadow             = c.base00;
+        hover              = c.base0B;
+        on_hover           = c.base00;
+      };
 
 		};
 
