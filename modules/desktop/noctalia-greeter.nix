53a668aa4f4c63bdf8ff811afc871147f82d6468
cqjro <github@cqjro.ca>
2026-09-04 18:04:41 -0400

    GOD-KILLER: 157         2026-09-04 18:04:39  26.11.20260828.83199d0  7.2.1   Unknown                 []              True || trying to fix the noctalia greeter theme

--- modules/desktop/noctalia-greeter.nix
+++ modules/desktop/noctalia-greeter.nix
@@ -23,24 +23,28 @@ in
 				layout = "us";
 			};
 
-			appearance.palette = {
-        primary            = c.base0D; # blue/accent
-        on_primary         = c.base00; # bg
-        secondary          = c.base0C; # cyan
-        on_secondary       = c.base00;
-        tertiary           = c.base0B; # green
-        on_tertiary        = c.base00;
-        error              = c.base08; # red
-        on_error           = c.base00;
-        surface            = c.base00; # bg
-        on_surface         = c.base05; # fg
-        surface_variant    = c.base01; # bg1
-        on_surface_variant = c.base04; # fg dim
-        outline            = c.base03;
-        shadow             = c.base00;
-        hover              = c.base0B;
-        on_hover           = c.base00;
-      };
+			appearance = {
+				scheme = "Synched";
+				theme_mode = "dark";
+				palette = {
+        	primary            = c.base0D; # blue/accent
+        	on_primary         = c.base00; # bg
+        	secondary          = c.base0C; # cyan
+        	on_secondary       = c.base00;
+        	tertiary           = c.base0B; # green
+        	on_tertiary        = c.base00;
+        	error              = c.base08; # red
+        	on_error           = c.base00;
+        	surface            = c.base00; # bg
+        	on_surface         = c.base05; # fg
+        	surface_variant    = c.base01; # bg1
+        	on_surface_variant = c.base04; # fg dim
+        	outline            = c.base03;
+        	shadow             = c.base00;
+        	hover              = c.base0B;
+        	on_hover           = c.base00;
+      	};
+			};
 
 		};
 
