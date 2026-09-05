5d6a9f425f0e214c567515fdb492f7fc8693782b
cqjro <github@cqjro.ca>
2026-09-05 12:59:04 -0400

    GOD-KILLER: 159         2026-09-05 12:59:01  26.11.20260828.83199d0  7.2.1   Unknown                 []              True || changing systemd status in hyprland to have uwsm work and also maybe steam ?

--- modules/desktop/hyprland.nix
+++ modules/desktop/hyprland.nix
@@ -139,7 +139,7 @@
     wayland.windowManager.hyprland = {
       enable = true;
       configType = "lua";
-      systemd.enable = true;
+      systemd.enable = false;
 
       settings = {
         config = {
